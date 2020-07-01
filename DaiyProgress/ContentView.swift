f393c15639917f64265eed105cb723c811baf960
Veit Progl <->
2020-07-01 21:11:13 +0200

    Initial Commit

---
+++ DaiyProgress/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DaiyProgress
+//
+//  Created by Veit Progl on 01.07.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
